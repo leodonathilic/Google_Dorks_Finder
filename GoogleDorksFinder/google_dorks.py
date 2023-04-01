564b6e87aec09f181e1e9134c5a4d2569dba5ccb
Léo DONATH-ILIC <129596282+leodonathilic@users.noreply.github.com>
2023-04-01 15:22:26 +0200

    V.1

--- GoogleDorksFinder/google_dorks.py
+++ GoogleDorksFinder/google_dorks.py
@@ -16,7 +16,7 @@ DORK_OUTPUT_FILE = open(f"DORK_{FILE_CREATION_DATE.strftime('%d-%m-%y_%I-%M-%S-%
 DORK = DORK.replace(' ', '%20')
 
 
-url = 'https://www.google.com/search?q='+DORK+'&rlz=1C1CHBF_frFR901FR901&oq='+DORK+'&aqs=chrome.0.69i59j0i20i131i263i433i457i512j0i402l2j0i20i263i433i512j0i433i512l2j0i67i433j0i433i512l2.1030j0j7&sourceid=chrome&ie=UTF-8'
+url = 'https://www.google.com/search?q='+DORK+'&rlz=1C1CHBF_frFR901FR901&oq='+DORK
 
 
 req = requests.get(url, headers = headers)
@@ -29,5 +29,5 @@ a_tags = divs.find_all('div', class_='yuRUbf')
 for div in a_tags:
 	FIND_LINK = div.find('a', href = True)
 	print(FIND_LINK.attrs['href'])
-	DORK_OUTPUT_FILE.write(FIND_LINK.attrs['href'])
+	DORK_OUTPUT_FILE.write(FIND_LINK.attrs['href'] + '\n')
 
